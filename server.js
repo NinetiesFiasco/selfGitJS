851eeec7d4a4f1aa9acdb473496244bcbf8c171d
Anton Seredin <seredin.anton2014@gmail.com>
2024-02-06 00:55:00 +0200

    git status

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const simpleGit = require('simple-git')
+const git = simpleGit.default()
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  res.send('Hi bro!')
+})
+
+app.get('/branch', async (req, res) => {
+  const branch = await git.branch()
+  res.send(branch)
+})
+
+app.get('/status', async (req, res) => {
+  const status = await git.status()
+  res.send(status)
 })
 
 app.listen(port, () => {
