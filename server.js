157c1f3ad0ec6e8e8156674a21ba744b00dfdbae
Anton Seredin <seredin.anton2014@gmail.com>
2024-02-06 01:22:36 +0200

    master update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', async (req, res) => {
-  res.send('Hi bro changes here!')
+  res.send('Hi bro changes here, may be they will work!')
 })
 
 app.get('/branch', async (req, res) => {
